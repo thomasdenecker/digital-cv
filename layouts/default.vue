deb835af4320258f0be1778382df9806f0be4297
Thomas Denecker <thomas.denecker@gmail.com>
2023-01-17 21:33:58 +0100

    Add quasar

---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <MySidebar />
+    <slot />
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import { quasar } from '@quasar/vite-plugin'
+
 export default defineNuxtConfig({
     target: 'static',
     router: {
         base: '/digital-cv/'
+    },
+    css: ['@quasar/extras/material-icons/material-icons.css',
+        '~/assets/styles/quasar.sass',
+    ],
+    build: {
+        transpile: ['quasar'],
+    },
+    vite: {
+        define: {
+            'process.env.DEBUG': false,
+        },
+        plugins: [
+            quasar({
+                sassVariables: 'assets/styles/quasar.variables.sass',
+            }),
+        ],
     }
+
 })
