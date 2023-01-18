368268bc056608d68c3a041047850a9af6a5a196
Thomas Denecker <thomas.denecker@gmail.com>
2023-01-18 18:23:19 +0100

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
     ssr: false,
     target: 'static',
     router: {
-        base: '/digital-cv/'
+        base: '/'
     },
     css: ['@quasar/extras/material-icons/material-icons.css',
         '~/assets/styles/quasar.sass',
